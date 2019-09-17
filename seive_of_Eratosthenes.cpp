bfe7f847dd8a1d000229cd85384e38a0526b18a9
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2019-09-17 06:56:17 +0530

    Create seive_of_Eratosthenes.cpp

---
+++ seive_of_Eratosthenes.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<bool>is_prime(n+1,true);
+    is_prime[0]=is_prime[1]=false;
+    
+    for(int i=2;i<=n;++i){
+        if(is_prime[i] and i*i<=n ){
+            for(int j=i*i;j<=n;j+=i)
+                is_prime[j]=false;
+        }
+    }
+    // Asymptotic complexity-- O(nlog(log(n)))
+    
+    for(int i=1;i<=n;++i)if(is_prime[i])cout<<i<<", ";
+    
+    return 0;
+}
