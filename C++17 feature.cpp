77a0ec0f19a527a0117f4184ecc330139b06093e
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2019-11-21 19:08:06 +0530

    Create C++17 feature.cpp

---
+++ C++17 feature.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl;
+template<typename... T>
+
+void read(T&... args){
+    ((cin>>args),...);
+}
+
+template<typename... T>
+void write(T... args){
+    ((cout<<args<<" "),...);
+}
+
+int main(){
+    
+    int a,b,c,d;
+    read(a,b,c,d);
+    write(a,b,c,d);
+    cout<<"\n";
+    deb(a);deb(b);
+    
+   
+}
